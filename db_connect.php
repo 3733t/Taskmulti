36c93969422a42a3efbd38d5704063221c8da800
Charlie <41951671+charlesgles9@users.noreply.github.com>
2023-07-10 15:35:59 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,10 @@
-<?php 
-
-$conn= new mysqli('localhost','root','','tms_db')or die("Could not connect to mysql".mysqli_error($con));
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:task-db.database.windows.net,1433; Database = tms_db", "task", "sunspot7301#");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
