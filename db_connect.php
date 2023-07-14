89bcbf0afbdb6ea4a7a0a4eeffbdb947e58e8517
charlie <charlesgles9@gmail.com>
2023-07-14 11:42:15 +0300

    updated connection string

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
-/*try {
+try {
     $conn = new PDO("sqlsrv:server = tcp:task-db.database.windows.net,1433; Database = tms_db", "task", "sunspot7301#");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
@@ -9,24 +9,8 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "task", "pwd" => "sunspot7301#", "Database" => "tms_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:task-db.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-*/
-$serverName = "tcp:task-db.database.windows.net,1433";
-$connectionOptions = array(
-    "Database" => "tms_db",
-    "Uid" => "task",
-    "PWD" => "sunspot7301#",
-    "LoginTimeout" => 30
-);
 
-try {
-    $conn = new PDO("sqlsrv:server = $serverName", $connectionOptions);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+
 
 ?>
